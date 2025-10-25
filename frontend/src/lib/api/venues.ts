b802f13909dee6af19d68bdf7e7375a6432ef893
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-10-24 20:10:55 -0500

    redid map file

--- frontend/src/lib/api/venues.ts
+++ frontend/src/lib/api/venues.ts
@@ -1,6 +1,22 @@
+interface Venue {
+    id: number;
+    name: string;
+    previous_names?: string;
+    city: string;
+    state: string;
+    latitude: number;
+    longitude: number;
+
+}
+
 const API_URL = "http://localhost:5187/api/Venue";
 
 export async function getAllVenues(fetch: typeof window.fetch) {
     const res = await fetch(`${API_URL}`);
     return res.json();
+}
+
+export async function getVenues(): Promise<Venue> {
+    const res = (await fetch(`${API_URL}`)).json();
+    return res;
 }
--- frontend/src/lib/components/Map.svelte
+++ frontend/src/lib/components/Map.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
 
-  let mapContainer: HTMLDivElement;
+    let mapContainer: HTMLDivElement;
 
-  onMount(async () => {
-    const L = await import('leaflet'); 
+    onMount(async () => {
+        const L = await import('leaflet');
 
-    const map = L.map(mapContainer).setView([37.7749, -122.4194], 3);
+        const map = L.map(mapContainer).setView([37.7749, -122.4194], 3);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    L.marker([37.7749, -122.4194])
-      .addTo(map)
-      .bindPopup('Hello from SvelteKit + Leaflet!')
-      .openPopup();
-
-  });
+        L.marker([38, -122])
+            .addTo(map)
+            .bindPopup('Hello from SvelteKit + Leaflet!')
+            .openPopup();
+    });
 </script>
 
-<div
-  bind:this={mapContainer}
-  class="h-[400px] w-full rounded-2xl shadow-md border border-gray-300"
+<div bind:this={mapContainer}
+    class="
+        border
+        border-gray-300
+        h-[400px]
+        rounded-2xl
+        shadow-md
+        w-full 
+    "
 ></div>
