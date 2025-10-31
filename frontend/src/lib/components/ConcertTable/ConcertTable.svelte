b45d7de2fe7b8518f93975848a55eddbb4f2c611
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-10-30 22:53:53 -0500

    updated table styling

--- frontend/src/lib/components/ConcertTable/ConcertTable.svelte
+++ frontend/src/lib/components/ConcertTable/ConcertTable.svelte
@@ -22,9 +22,16 @@
 ">
 
     <div class="h-full overflow-auto">
-        <table class="border-collapse min-w-full" >
-            <thead class="bg-white sticky top-0 z-10">
-                <tr>
+        <table class="border-collapse min-w-full">
+            <thead class="
+                backdrop-blur
+                bg-white/90
+                shadow-[0_2px_0_0_rgba(0,0,0,0.1)]
+                sticky 
+                top-0 
+                z-10
+            ">
+                <tr class="hover:bg-green-100">
                     <th class="text-left px-4 py-2">Date</th>
                     <th class="text-left px-4 py-2">Band</th>
                     <th class="text-left px-4 py-2">Venue</th>
@@ -32,7 +39,7 @@
             </thead>
             <tbody>
                 {#each concertSummaries as c}
-                    <tr class="border-t">
+                    <tr class="border-t hover:bg-green-100">
                         <td class="px-4 py-2">{c.date}</td>
                         <td class="px-4 py-2">{c.headliners}</td>
                         <td class="px-4 py-2">{c.venueName}</td>
