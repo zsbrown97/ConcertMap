50c6c97a342daf030f694286055eca43e006d892
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-10-31 10:12:13 -0500

    backend port change and added api url to env

--- frontend/src/lib/api/concerts.ts
+++ frontend/src/lib/api/concerts.ts
@@ -1,6 +1,6 @@
-const API_URL = "http://localhost:5187/api/Concert";
+import { PUBLIC_API_URL } from "$env/static/public";
 
 export async function getConcertSummaries(fetch: typeof window.fetch) {
-    const res = await fetch(`${API_URL}`)
+    const res = await fetch(`${PUBLIC_API_URL}/Concert`)
     return res.json();
 }
--- frontend/src/lib/api/venues.ts
+++ frontend/src/lib/api/venues.ts
@@ -1,6 +1,5 @@
-const API_URL = "http://localhost:5187/api/Venue";
-
+import { PUBLIC_API_URL } from "$env/static/public";
 export async function getAllVenues(fetch: typeof window.fetch) {
-    const res = await fetch(`${API_URL}`);
+    const res = await fetch(`${PUBLIC_API_URL}/Venue`);
     return res.json();
 }
